e83c9d9f4746b0f7c802e542543a71f2d66894c9
Matty-Montoya <mmontoya92@gmail.com>
2018-04-05 09:08:00 -0400

    Change to time out Ui settimeout
    Made changes to settimeout

--- assets/scripts/user-actions/ui.js
+++ assets/scripts/user-actions/ui.js
@@ -3,20 +3,20 @@
 const store = require('../store.js')
 
 const signUpSuccess = function () {
-  $('#status').html('You have successfully signed up! Please sign in!')
-  // setTimeout(() => { $('#status').fadeOut() }, 3000)
+  // $('#status').html('You have successfully signed up! Please sign in!')
+  setTimeout(() => $('#status').html('You have successfully signed up! Please sign in!'), 2000)
   $('#myModal1').modal('toggle')
 }
 
 const signUpFailure = function () {
-  $('.authmessage').html('Please try again!')
-  // setTimeout(() => { $('.authmessage').fadeOut() }, 3000)
+  // $('.authmessage').html('Please try again!')
+  setTimeout(() => $('.authmessage').text('Please try again!'), 2000)
 }
 
 const signInSuccess = function (data) {
   console.log(data)
-  $('#status').html('You have successfully signed in!')
-  // setTimeout(() => { $('#status').fadeOut() }, 3000)
+  // $('#status').html('You have successfully signed in!')
+  setTimeout(() => $('#status').text('You have successfully signed in!'), 2000)
   $('#myModal').modal('toggle')
   $('.sign-in').addClass('hidden')
   $('.sign-up').addClass('hidden')
@@ -26,24 +26,24 @@ const signInSuccess = function (data) {
 }
 
 const signInFailure = function () {
-  $('.authmessage').text('Please try again!')
-  // setTimeout(() => { $('.authmessage').fadeOut() }, 3000)
+  // $('.authmessage').text('Please try again!')
+  setTimeout(() => $('.authmessage').text('Please try again!'), 2000)
 }
 
 const changePasswordSuccess = function () {
-  $('#status').text('You have successfully changed password!')
-  // setTimeout(() => { $('#status').fadeOut() }, 3000)
+  // $('#status').text('You have successfully changed password!')
+  setTimeout(() => $('#status').text('You have successfully changed password!'), 2000)
   $('#myModal2').modal('toggle')
 }
 
 const changePasswordFailure = function () {
-  $('.authmessage').html('Please try again!')
-  // setTimeout(() => { $('.authmessage').fadeOut() }, 3000)
+  // $('.authmessage').html('Please try again!')
+  setTimeout(() => $('.authmessage').text('Please Try Again!'), 2000)
 }
 
 const signOutSuccess = function () {
-  $('#status').text('You have signed out!')
-  // setTimeout(() => { $('#status').fadeOut() }, 3000)
+  // $('#status').text('You have signed out!')
+  setTimeout(() => $('#status').text('You have signed out!'), 2000)
   $('.sign-in').removeClass('hidden')
   $('.sign-up').removeClass('hidden')
   $('.change-password').addClass('hidden')
