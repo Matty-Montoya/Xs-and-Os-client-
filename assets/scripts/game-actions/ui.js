ecec290ce84c861b0e999ce500649c2c3e97b93e
Matty-Montoya <mmontoya92@gmail.com>
2018-04-19 16:36:28 -0400

    Print: Print the total games played on Get index
    Able to link the the fetch games button to print the amount of games.

--- assets/scripts/game-actions/ui.js
+++ assets/scripts/game-actions/ui.js
@@ -7,14 +7,15 @@ const gameStartSuccess = function (data) {
   $('#counter').removeClass('hidden')
   $('#replay').removeClass('hidden')
   $('#game-board').removeClass('hidden')
-
   store.game = data.game
 }
 
 const gameIndexSuccess = function (data) {
   console.log('data is', data)
   const length = data.games.length
-  console.log(length)
+  $('#games-played').removeClass('hidden')
+  $('#games-played').text('Total Games Played ' + length)
+  setTimeout(() => $('#games-played').text(''), 4000)
 }
 
 const gamePatchSuccess = function (data) {
