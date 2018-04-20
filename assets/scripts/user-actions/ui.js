e2f9535dd07df9a8eff4da31405e66e2c110668f
Matty-Montoya <mmontoya92@gmail.com>
2018-04-20 15:05:04 -0400

    Feature: Successful Form Reset. Removed Footer
    Form Reset
    -Resolved the issue regarding where form fields were not resetting.
    -All form fields will now reset
    
    Footer Removal
    -Too many issues with deployed version of game with footer
    -Remove footer and CSS properties assigned to footer.

--- assets/scripts/user-actions/ui.js
+++ assets/scripts/user-actions/ui.js
@@ -6,11 +6,15 @@ const signUpSuccess = function () {
   $('#status').text('You have successfully signed up! Please sign in!')
   setTimeout(() => $('#status').text(''), 2000)
   $('#myModal1').modal('toggle')
+  $('input[type=text]').val('')
+  $('input[type=password]').val('')
 }
 
 const signUpFailure = function () {
   $('.authmessage1').text('Please try again!')
   setTimeout(() => $('.authmessage1').text(''), 2000)
+  $('input[type=text]').val('')
+  $('input[type=password]').val('')
 }
 
 const signInSuccess = function (data) {
@@ -28,23 +32,31 @@ const signInSuccess = function (data) {
   $('#get-games').removeClass('hidden')
   $('#counter').removeClass('hidden')
   $('#counter').text('What would you like to do?')
+  $('input[type=text]').val('')
+  $('input[type=password]').val('')
   store.user = data.user
 }
 
 const signInFailure = function () {
   $('.authmessage').text('Please try again!')
   setTimeout(() => $('.authmessage').html(''), 5000)
+  $('input[type=text]').val('')
+  $('input[type=password]').val('')
 }
 
 const changePasswordSuccess = function () {
   $('#status').text('You have successfully changed password!')
   setTimeout(() => $('#status').text(''), 2000)
   $('#myModal2').modal('toggle')
+  $('input[type=text]').val('')
+  $('input[type=password]').val('')
 }
 
 const changePasswordFailure = function () {
   $('.authmessage2').text('Please try again!')
   setTimeout(() => $('.authmessage2').text(''), 2000)
+  $('input[type=text]').val('')
+  $('input[type=password]').val('')
 }
 
 const signOutSuccess = function () {
@@ -60,6 +72,8 @@ const signOutSuccess = function () {
   $('#counter').addClass('hidden')
   $('#play').addClass('hidden')
   $('#get-games').addClass('hidden')
+  $('input[type=text]').val('')
+  $('input[type=password]').val('')
   store.user = null
 }
 
